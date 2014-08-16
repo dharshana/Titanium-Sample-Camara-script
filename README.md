25b1b0b0ced91aaed0f1e9f61acd08b3197167e5
Dharshana Jayamaha <dharshanajayamaha@yahoo.com>
2014-08-16 19:52:44 +0530

    Camera Script

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-Titanium-Sample-Camara-script
+Titanium Camara script
 =============================
 
-This Contatin sample Script to take a image from phone camera
+Sample Titanium Script to pop up your camera and take a photograph
+Script work for both Android and iOS devices
+
+
+requirements
+Titanium SDK 3.X / 2.X
+Titanium Studio
+Android SDK 2.3+
+Android Simiulator
+
+Application Type : mobile
+Supported Titanium SDK : 3.x
+Platform & versions
+	Android 2.3.3 (API 10)
+	Android 3.0 (API 11)
+	Android 4.0 (API 14)
+	Android 4.2 (API 17)
+Devices: iOS simulator, Android emulator or physical devices
+Operating System : Win, Linux, Mac
+
+
+Tested Envoinment
+Android 2.3.3 (API 10)
+Android 3.0 (API 11)
+Android 4.0 (API 14)
+Android 4.2 (API 17)
+Windows 7
+Windows 8
+
--- src/Resources/functions.js
+++ src/Resources/functions.js
@@ -1,5 +1,4 @@
 btntakepic.addEventListener('click', function(){
-	//Ti.API.info('Hello this is from cam');
 	fireUpTheCamera();
 });
 imageholder.addEventListener('click', function(){
@@ -8,102 +7,6 @@ imageholder.addEventListener('click', function(){
 
 btnsave.addEventListener('click', function(){
 	Ti.API.info('Save image');
-	
-	var imageblob = imageholder.toBlob();
-     console.log(imageblob) ;
-     
-    var lbl = Ti.UI.createLabel({
-		text:'Uploading...',
-		font: {fontSize:24}, color: '#000'
-	});
-	imageholder.opacity = 0.3;
-	btnsave.opacity = 0.3;
-	btncancel.opacity = 0.3;
-	btntakepic.opacity = 0.3;
-	imageview.add(lbl);
-	
-	btnsave.enabled = false;
-	btncancel.enabled = false;
-	btntakepic.enabled = false;
-	
-     var client = Ti.Network.createHTTPClient({
-            onload : function(e){
-            	lbl.text = 'Send';
-            	Ti.API.info('Image SEND >>> '+client.responseText);
-           		imageholder.opacity = 1.0;
-				btnsave.opacity = 1.0;
-				btncancel.opacity = 1.0;
-				btntakepic.opacity = 1.0;
-				
-				imageholder.image = 'tap.png';
-				
-				btnsave.enabled = true;
-				btncancel.enabled = true;
-				btntakepic.enabled = true;
-				lbl.text = '';
-            },
-            onerror : function(e){
-            	alert('error in connection');
-            	imageholder.opacity = 1.0;
-				btnsave.opacity = 1.0;
-				btncancel.opacity = 1.0;
-				btntakepic.opacity = 1.0;
-				lbl.text = '';
-				imageholder.image = 'tap.png';
-				
-				btnsave.enabled = true;
-				btncancel.enabled = true;
-				btntakepic.enabled = true;
-            },
-            timeout : 60000
-        });
-        client.open('POST', '174.121.38.94/~lemzonl/rad/1.php');
-        client.setRequestHeader("Content-type","multipart/form-data");
-        //client.setRequestHeader("Content-type","multipart/form-data");
-        client.setRequestHeader("Content-type","image/png");
-        //client.send(imageblob);
-        client.send({"name" : 'filename'});
-	
-	// var xhr = Ti.Network.createHTTPClient ();
-	// var lbl = Ti.UI.createLabel({
-		// text:'Uploading...',
-		// font: {fontSize:24}, color: '#000'
-	// });
-	// imageholder.opacity = 0.3;
-	// btnsave.opacity = 0.3;
-	// btncancel.opacity = 0.3;
-	// btntakepic.opacity = 0.3;
-	// imageview.add(lbl);
-// 	
-	// btnsave.enabled = false;
-	// btncancel.enabled = false;
-	// btntakepic.enabled = false;
-// 
-	  // //xhr.open ( "POST", '173.254.36.205' );
-	  // xhr.open ( "POST", '174.121.38.94/~lemzonl/rad/1.php' );
-	  // //xhr.setTimeout ( 20000 );
-	  // xhr.send ( 
-	  // {         
-	    // //"CommandType"    : "image",
-	    // //"file"           : e.media,
-	    // "name"           : 'filename'
-	  // });
-// 	
-	  // xhr.onload = function (e)
-	  // {
-	    // Ti.API.info ("image sent to server");
-	    // imageholder.opacity = 1.0;
-		// btnsave.opacity = 1.0;
-		// btncancel.opacity = 1.0;
-		// btntakepic.opacity = 1.0;
-		// lbl.text = '';
-		// imageholder.image = 'tap.png';
-// 		
-		// btnsave.enabled = true;
-		// btncancel.enabled = true;
-		// btntakepic.enabled = true;
-	  // }
-});
 
 btncancel.addEventListener('click', function(){
 		imageholder.image = 'tap.png';
@@ -120,14 +23,7 @@ function fireUpTheCamera() {
  
             Ti.API.debug('Our type was: ' + event.mediaType);
             if (event.mediaType == Ti.Media.MEDIA_TYPE_PHOTO) {
-                // var imageView = Ti.UI.createImageView({
-                    // width : win.width,
-                    // height : win.height,
-                    // image : event.media
-                // });
-                //win.add(imageView);
  				imageholder.image = event.media;
- 				 //Titanium.UI.orientation = Titanium.UI.PORTRAIT;
  				
             } else {
                 alert("got the wrong type back =" + event.mediaType);
